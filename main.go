b400f3975b6fcec879a9798389654ade4a07e3b4
Peyman <hassani.peyman627@gmail.com>
2024-10-25 12:36:23 +0330

    Add keyed items in array

--- main.go
+++ main.go
@@ -13,7 +13,24 @@ func main() {
 	//_ = sportsCar.Drive()
 
 	// Arg indexing starts from 1
-	var planet = "venus"
-	var distance = 261
-	fmt.Printf("%v is %v km away from us. think! %[2]v away is %[1]v", planet, distance)
+	//var planet = "venus"
+	//var distance = 261
+	//fmt.Printf("%v is %v km away from us. think! %[2]v away is %[1]v\n", planet, distance)
+
+	// keyed items in array initialization
+	count := 0
+	for range [...]int{9: 0} {
+		count += 1
+	}
+	fmt.Println(count)
+
+	isVowel := []bool{'a': true, 'e': true, 'i': true, 'o': true, 'u': true}
+	fmt.Println(isVowel)
+
+	isWeekend := []bool{5: true, 6: true}
+	fmt.Println(isWeekend)
+
+	// Confusion
+	a := [...]int{5, 4: 1, 0, 2: 3, 2, 1: 4}
+	fmt.Println(a)
 }
