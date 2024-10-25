eba3f15524dd9a2157edc9b951724ebbcbf7193b
Peyman <hassani.peyman627@gmail.com>
2024-10-25 12:20:01 +0330

    Add arg indexing example in printf

--- main.go
+++ main.go
@@ -1,14 +1,19 @@
 package main
 
-import "github.com/peyzor/golab/car"
+import "fmt"
 
 func main() {
 	//Builder Pattern usage
-	assembly := car.NewBuilder().Color(car.GreenColor)
+	//assembly := car.NewBuilder().Color(car.GreenColor)
+	//
+	//familyCar := assembly.Wheels(car.SportsWheels).TopSpeed(50 * car.MPH).Build()
+	//_ = familyCar.Drive()
+	//
+	//sportsCar := assembly.Wheels(car.SteelWheels).TopSpeed(150 * car.MPH).Build()
+	//_ = sportsCar.Drive()
 
-	familyCar := assembly.Wheels(car.SportsWheels).TopSpeed(50 * car.MPH).Build()
-	_ = familyCar.Drive()
-
-	sportsCar := assembly.Wheels(car.SteelWheels).TopSpeed(150 * car.MPH).Build()
-	_ = sportsCar.Drive()
+	// Arg indexing starts from 1
+	var planet = "venus"
+	var distance = 261
+	fmt.Printf("%v is %v km away from us. think! %[2]v away is %[1]v", planet, distance)
 }
