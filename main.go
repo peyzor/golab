a695fe029123388beb5f04698beaec3f03ceeba4
Peyman <hassani.peyman627@gmail.com>
2024-10-27 15:20:39 +0330

    Add changing data in slice

--- main.go
+++ main.go
@@ -14,4 +14,22 @@ func main() {
 	p.x = 10
 	//(*p).x = 10 // the language automatically does this for us
 	fmt.Printf("%+v\n", p)
+
+	names := [4]string{
+		"John",
+		"George",
+		"Paul",
+		"Ringo",
+	}
+
+	fmt.Println(names)
+
+	a := names[0:2]
+	b := names[1:]
+	fmt.Println(a, b)
+
+	// changing data in slice changes the data in the underlying array
+	b[0] = "XXX"
+	fmt.Println(a, b)
+	fmt.Println(names)
 }
