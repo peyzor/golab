240372f115927cfe44ab3e10aafe8b5244b58d18
Peyman <hassani.peyman627@gmail.com>
2024-10-27 15:15:22 +0330

    Add automatic pointer dereferencing

--- main.go
+++ main.go
@@ -2,6 +2,16 @@ package main
 
 import "fmt"
 
+type Vertex struct {
+	x int
+	y int
+}
+
 func main() {
-	fmt.Println("new experiments here")
+	// struct fields can be accessed through a struct pointer
+	v := Vertex{1, 2}
+	p := &v
+	p.x = 10
+	//(*p).x = 10 // the language automatically does this for us
+	fmt.Printf("%+v\n", p)
 }
