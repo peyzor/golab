1f47680f9242aee936526f8dfcf52a4509fa4f57
Peyman <hassani.peyman627@gmail.com>
2024-10-28 11:38:11 +0330

    Add empty interface, type casting and type assertions

--- main.go
+++ main.go
@@ -3,5 +3,26 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("new experiments here")
+	//var i interface{}
+	// the empty interface can hold any value of any type
+	var i any
+
+	i = 42
+	fmt.Printf("%v\n", i)
+	i = "xd"
+	fmt.Printf("%v\n", i)
+
+	// type casting
+	i = float64(10)
+	fmt.Printf("(%v, %[1]T)\n", i)
+
+	// a type assertion provides access to an interface value's underlying concrete type
+	myFloat := i.(float64)
+	fmt.Printf("(%v, %[1]T)\n", myFloat)
+
+	s, ok := i.(string)
+	fmt.Printf("(%v, %[1]T), %t\n", s, ok)
+
+	v := i.(string) // panic
+	fmt.Printf("(%v, %[1]T)\n", v)
 }
