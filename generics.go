ec8710047266a9e5b63c26a2f8c50fc3aee4fefc
Peyman <hassani.peyman627@gmail.com>
2024-10-28 13:01:54 +0330

    Add first generic type

--- generics.go
+++ generics.go
@@ -12,6 +12,27 @@ func Index[T comparable](s []T, x T) int {
 	return -1
 }
 
+type Node[T any] struct {
+	next *Node[T]
+	val  T
+}
+
+func (n *Node[T]) print() {
+	head := n
+	for head != nil {
+		fmt.Println(head.val)
+		head = head.next
+	}
+}
+
+func printFunc[T any](n *Node[T]) {
+	head := n
+	for head != nil {
+		fmt.Println(head.val)
+		head = head.next
+	}
+}
+
 func generics() {
 	si := []int{1, 2, 3, 4, 5}
 	result := Index(si, 3)
@@ -20,4 +41,10 @@ func generics() {
 	si2 := []string{"holy", "moly", "xdd"}
 	result = Index(si2, "moly")
 	fmt.Printf("first index appeared is: %v\n", result)
+
+	x3 := Node[int]{nil, 3}
+	x2 := Node[int]{&x3, 2}
+	x1 := Node[int]{&x2, 1}
+	x1.print()
+	printFunc(&x1)
 }
